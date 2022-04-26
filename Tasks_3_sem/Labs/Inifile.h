b7ded662d0ec009674fb1fa64ec60ebef8480d98
Манана <76215627+millysmask02@users.noreply.github.com>
2022-04-26 11:33:37 +0300

    Added tests for "Inifile"

--- Tasks_3_sem/Labs/Inifile.h
+++ Tasks_3_sem/Labs/Inifile.h
@@ -1,6 +1,5 @@
 class IniFile {
 	std::map<std::string, std::map<std::string, std::string>> _data;
-
 public:
 	IniFile(std::string path);
 
@@ -36,6 +35,7 @@ public:
 windowWidth=1080
 windowHeight=720
 fullscreen=Yes
+
 [player]
 x=100
 y=200
@@ -47,38 +47,13 @@ name=John Doe
 windowWidth=1080
 windowHeight=720
 fullscreen=Yes
+
 [player]
 x=100
 y=200
 name=Ivan Petrov Vasilevishcyhhsh
 bool=true
+
 [newSection]
 value=137
 */
-
-
-int main(int argc, char** argv) {
-    IniFile cfg("config.ini");
-
-    assert(cfg.read<bool>("general", "fullscreen") == true);
-    assert(cfg.read<std::string>("player", "name") == "John Doe");
-    assert(cfg.read<int>("player", "x") == 100);
-    assert(cfg.read<int>("player", "y") == 200);
-
-    assert(cfg.read<int>("player", "z") == 0);
-    assert(cfg.read<int>("player", "z", 500) == 500);
-
-    assert(cfg.sectionExists("general") == true);
-    assert(cfg.sectionExists("player") == true);
-    assert(cfg.sectionExists("AAAAAA") != true);
-
-    assert(cfg.keyExists("general", "windowWidth") == true);
-    assert(cfg.keyExists("general", "windowHeight") == true);
-    assert(cfg.keyExists("general", "windowDepth") == false);
-    assert(cfg.keyExists("asdasdadasd", "windowWidth") == false);
-
-    cfg.write<std::string>("player", "name", "Ivan Petrov Vasilevishcyhhsh");
-    cfg.write<bool>("player", "bool", true);
-    cfg.write<int>("newSection", "value", 137);
-    cfg.save("config.ini");
-}
