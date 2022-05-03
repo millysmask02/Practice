364204a01b9003bdcc38e4dbf23d4038ca0575f0
U1tr0 <75480853+U1tr0@users.noreply.github.com>
2022-05-03 12:03:56 +0300

    Added full test for List

--- Tasks_2_sem/Labs/LinkedList_test.cpp
+++ Tasks_2_sem/Labs/LinkedList_test.cpp
@@ -2,11 +2,21 @@
 #include "catch.hpp"
 #include "Header.h"
 
+void add5(double& a) {
+    a = a + 5;
+}
 
+bool moreThen8(double a) {
+    return a > 8;
+}
+
+double add5(double a) {
+    return a + 5;
+}
 
 TEST_CASE("List")
 {
-    SECTION(" ") {
+    SECTION("Default functions") {
         LinkedList test1 = LinkedList();
         test1.insert(0, 1);
         test1.insert(1, 2);
@@ -27,7 +37,7 @@ TEST_CASE("List")
         test3.pushFront(3);
         test3.pushFront(2);
         test3.pushFront(1);
-        
+
         REQUIRE(test1.size() == 5);
         for (int i = 0; i < test1.size(); i++) {
             REQUIRE(test1.getNode(i)->_value == test2.getNode(i)->_value);
@@ -58,104 +68,64 @@ TEST_CASE("List")
             REQUIRE(test3.findIndex(i) == i - 1);
             REQUIRE(test3.getNode(i) == test3.findNode(i + 1));
         }
+    }
 
-        /*
-        std::vector<int> res = { 5, 5, 5 };
-        for (int i = 0; i < test.size(); i++) {
-            REQUIRE(test[i] == res[i]);
-        }
-        REQUIRE(test.size() == 3);
-        REQUIRE(test.capacity() == 9);
+    SECTION("Advanced functions") {
+        LinkedList test1 = LinkedList();
+        test1.insert(0, 1);
+        test1.insert(1, 2);
+        test1.insert(2, 3);
+        test1.insert(3, 4);
+        test1.insert(4, 5);
 
-        MyVector<int> test1 = MyVector<int>(2, 1, ResizeStrategy::Additive, 3);
-        std::vector<int> res1 = { 1, 1 };
+        LinkedList test2 = LinkedList();
+        test2.insert(0, 5);
+        test2.insert(1, 4);
+        test2.insert(2, 3);
+        test2.insert(3, 2);
+        test2.insert(4, 1);
+
+        test2.reverse();
         for (int i = 0; i < test1.size(); i++) {
-            REQUIRE(test1[i] == res1[i]);
+            REQUIRE(test1[i] == test2[i]);
         }
+
+        LinkedList test3 = test2.getReverseList();
+        LinkedList test4 = test3.getReverseList();
+        for (int i = 0; i < test1.size(); i++) {
+            REQUIRE(test2[i] == test4[i]);
+        }
+
+        REQUIRE(test4.begin() == test4.getNode(0));
+        REQUIRE(test4.end() == nullptr);
+
+    }
+
+    SECTION("Special functions") {
+        LinkedList test1 = LinkedList();
+        test1.insert(0, 1);
+        test1.insert(1, 2);
+        test1.insert(2, 3);
+        test1.insert(3, 4);
+        test1.insert(4, 5);
+
+        LinkedList test2 = LinkedList();
+        test2.insert(0, 1);
+        test2.insert(1, 2);
+        test2.insert(2, 3);
+        test2.insert(3, 4);
+        test2.insert(4, 5);
+        test2.map(add5);
+
+        test1.forEach(add5);
+        for (int i = 0; i < test1.size(); i++) {
+            REQUIRE(test1[i] == i + 6);
+            REQUIRE(test2[i] == i + 6);
+        }
+
+        test1.filter(moreThen8);
         REQUIRE(test1.size() == 2);
-        REQUIRE(test1.capacity() == 5);
-
-        std::vector<int> vec_ins = { 5, 1, 1, 5, 5 };
-        test.insert(1, test1);
-        for (int i = 0; i < test.size(); i++) {
-            REQUIRE(test[i] == vec_ins[i]);
-        }
-
-        test.insert(3, 4);
-        test.insert(5, 7);
-        test.insert(2, 3);
-        std::vector<int> ins = { 5, 1, 3, 1, 4, 5, 7, 5 };
-        for (int i = 0; i < test.size(); i++) {
-            REQUIRE(test[i] == ins[i]);
-        }
-
-        test.erase(4, 2);
-        test.pushFront(9);
-        std::vector<int> push = { 9, 5, 1, 3, 1, 7, 5 };
-        for (int i = 0; i < test.size(); i++) {
-            REQUIRE(test[i] == push[i]);
-        }
-    }
-
-    SECTION("Constructors") {
-        MyVector<int> test(1, ResizeStrategy::Additive, 6);
-        REQUIRE(test[0] == 0);
-        test.pushBack(1);
-        test.pushFront(3);
-        MyVector<int> tcopy(test);
-        for (int i = 0; i < test.size(); i++) {
-            REQUIRE(tcopy[i] == test[i]);
-        }
-
-        MyVector<int> tOperCopy = test;
-        for (int i = 0; i < test.size(); i++) {
-            REQUIRE(tOperCopy[i] == test[i]);
-        }
-
-        MyVector<int> vec(std::move(test));
-        REQUIRE(test.size() == 0);
-        for (int i = 0; i < tOperCopy.size(); i++) {
-            REQUIRE(tOperCopy[i] == vec[i]);
-        }
-    }
-
-    SECTION("size and capacity") {
-        MyVector<int> test(3, 5, ResizeStrategy::Additive, 6);
-        REQUIRE(test.loadFactor() == 0.333333);
-
-        test.resize(5, 1);
-        std::vector<int> vec = { 5,5,5,1,1 };
-        for (int i = 0; i < test.size(); i++) {
-            REQUIRE(vec[i] == test[i]);
-        }
-
-        test.reserve(10);
-        REQUIRE(test.capacity() == 10);
-
-        test.clear();
-        REQUIRE(test.size() == 0);
-
-        REQUIRE(test[2] == 5);
-    }
-
-    SECTION("insert/erase") {
-        MyVector<int> test(2, 1, ResizeStrategy::Additive, 10);
-        MyVector<int> ins(2, 2, ResizeStrategy::Additive, 4);
-        test.insert(0, 4);
-        test.insert(3, 5);
-        test.insert(3, ins);
-        std::vector<int> res = { 4, 1, 1, 2, 2, 5 };
-        for (int i = 0; i < test.size(); i++) {
-            REQUIRE(res[i] == test[i]);
-        }
-
-        test.popBack();
-        test.erase(1);
-        test.erase(2, 2);
-        std::vector<int> vecEr = { 4,1 };
-        for (int i = 0; i < test.size(); i++) {
-            REQUIRE(vecEr[i] == test[i]);
-        }
-    */    
+        REQUIRE(test1[0] == 9);
+        REQUIRE(test1[1] == 10);        
     }
 }
--- Tasks_2_sem/Labs/List.h
+++ Tasks_2_sem/Labs/List.h
@@ -1,92 +1,80 @@
-#include <cstddef>
-#include <stdexcept>
-#include <utility>
 #include <iostream>
-#include <iterator>
-#include <cstddef>  
 
-using ValueType = int;
-class List {
-    class Node {
-    public:
-    	Node(const ValueType& value, Node* next = nullptr);
-		~Node();
+using ValueType = double;
+
+class LinkedList
+{
+	struct Node {
+		Node(const ValueType& value, Node* next = nullptr);
+		~Node() = default;
+
 		void insertNext(const ValueType& value);
 		void removeNext();
 
-		ValueType data;
-		Node* next;
-    };
-    Node* _head;
-    size_t _size;
+		ValueType _value;
+		Node* _next;
+	};
+
+	struct Iterator {
+	public:
+		Iterator(Node* ptr, size_t idx = 0);
+		Iterator(const Iterator& other);
+		Iterator& operator=(const Iterator& other);
+		ValueType& operator*() const;
+		bool operator!=(const Iterator& other) const;
+		bool operator==(const Iterator& other) const;
+		Iterator& operator++();
+		Iterator operator++(int);
+
+		size_t getIndex() const;
+		Node* getPtr() const;
+	private:
+		Node* _ptr;
+		size_t _index;
+	};
+
 public:
-    class Iterator;
-    List();
-    List(const List& copy); //конструктор копирования
-    List(List&& other) noexcept; //конструктор перемещения
+	Iterator begin(); 
+	Iterator end() noexcept; 
 
-    List& operator=(const List& copy);
-    List& operator=(List&& other) noexcept;
+	LinkedList(); 
+	LinkedList(const LinkedList& copyList); 
+	LinkedList& operator=(const LinkedList& copyList); 
 
-    ~List() = default;
+	LinkedList(LinkedList&& moveList) noexcept; 
+	LinkedList& operator=(LinkedList&& moveList) noexcept; 
 
-    size_t size() const;
-    bool empty() const; //пустой ли список
+	~LinkedList();
 
-    const ValueType& operator[](size_t pos) const; // доступ к значению элемента по индексу
-    ValueType& operator[](size_t pos);
-	List::Node* getNode(const size_t pos) const; // доступ к узлу по индексу
+	const ValueType& operator[](const size_t pos) const; 
+	ValueType& operator[](const size_t pos); 
+	LinkedList::Node* getNode(const size_t pos) const; 
 
-    const ValueType& front() const; // возвращает ссылку на первый элемент
-    ValueType& front();
+	void insert(const size_t pos, const ValueType& value); 
+	void insertAfterNode(Node* node, const ValueType& value);
+	void pushBack(const ValueType& value); 
+	void pushFront(const ValueType& value); 
 
-    const ValueType& back() const; // возвращает ссылку на последний элемент
-    ValueType& back();
+	void remove(const size_t pos); 
+	void removeNextNode(Node* node); 
+	void removeFront(); 
+	void removeBack(); 
 
-    void insert(size_t pos, const ValueType& value);
-    
-    void pushBack(const ValueType& value);
-    void pushFront(const ValueType& value);
+	long long int findIndex(const ValueType& value) const; 
+	Node* findNode(const ValueType& value) const; 
 
-    void popFront();
-    void popBack();
-    void remove(size_t pos);
-    
+	void reverse(); 
+	LinkedList getReverseList() const; 
 
-    void clear();
+	size_t size() const; 
+	void clear(); 
 
-    // void forEach(void (* callback)(ValueType& currVal, size_t idx));
-    // List map(ValueType (* callback)(const ValueType& currVal)) const;
-    // List filter(bool (* callback)(const ValueType& currVal)) const;
-    // void insert(const List::Iterator& pos, const ValueType& val);
-    // void remove(const Iterator& pos);
-    // void removeAfter(const Iterator& pos);
+	void forEach(void (*fn)(ValueType&)); 
+	void map(ValueType(*fn)(ValueType)); 
+	void filter(bool (*fn)(ValueType)); 
+private:
+	Node* _head;
+	size_t	_size;
 
-    void reverse(size_t begin, size_t end);
-
-    void print();
-
-
-    class Iterator {
-    private:
-        Node* _ptr;
-    public:
-        friend List;
-        Iterator(Node* ptr);
-        Iterator(const Iterator& it);
-
-        Iterator operator+(size_t n);
-
-        Iterator& operator++ ();
-
-        Iterator operator++ (int);
-
-
-        bool operator!=(const Iterator& it) const;
-        bool operator==(const Iterator& it ) const;
-
-        ValueType& operator*() const;
-    };
-    Iterator begin() const { return Iterator(_head); };
-    Iterator end() const { return nullptr; };
+	void forceNodeDelete(Node* node);
 };
